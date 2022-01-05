97d48577c052527a680382c2e1df4fad9940558f
Meg <mrb21@st-andrews.ac.uk>
2022-01-05 16:45:04 +0000

    identifying linked articles correctly

--- scraper.py
+++ scraper.py
@@ -3,17 +3,7 @@ from bs4 import BeautifulSoup
 from wikidata.client import Client
 from wikimapper import WikiMapper
 import concurrent.futures
-import pandas as pd
-import matplotlib.pyplot as plt
-import lxml
-import networkx as nx
-import cchardet
-import time 
-from multiprocessing import Pool 
-from itertools import compress
-
-from concurrent.futures import ThreadPoolExecutor as PoolExecutor
-from concurrent.futures import Future
+import time
 
 start_time = time.time()
 
@@ -35,21 +25,29 @@ is a human or not (only want links to other humans)
 """
 def isName(id):
     client = Client()
+    entityCompare = client.get('Q268702',load=True)
+    instance_of = client.get('P31', load=True)
+    typesCompare = entityCompare.getlist(instance_of)
+    for t in typesCompare:
+        t.load()
 
-    # assertion error : assert self.data is not None
-    print(id)
-
-    # some weird looking links that need chucking out so we are only left with proper links
-
-    if id:
+    try:
         entity = client.get(id, load=True)
         instance_of = client.get('P31', load=True)
+        # breaks after here
         types = entity.getlist(instance_of)
         for t in types:
             t.load()
-        print(types)
+        result = 0
+        if(len(types) > 0):
+            result = (types[0] == typesCompare[0])
+        return result, id
 
-    return False
+    except Exception as inst:
+        print(type(inst))
+        print(id)
+
+    return False, id
 
 """
 kinda the driver method but should send requests out
@@ -74,71 +72,46 @@ def requestPage(URL):
     for link in soup.find(id="bodyContent").find_all("a"):
         url = link.get("href", "")
         # only looking for the links in the article that are wiki links atm 
-        if "/wiki/" in url and "/wiki/Category" not in url:
+        if url.startswith("/wiki/") and "/wiki/Category" not in url:
             links[link.text.strip()] = url
 
     # need to go through and get all the links that are humans
     
     values = list(links.values())
-    keys1 = list(links.keys())
-    length = len(values)
-
-    keys = ["https://en.wikipedia.org" + i for i in values]
+    values = ["https://en.wikipedia.org" + i for i in values]
 
     # still very fast :)
-    mapToWikiData(keys)
+    keys = mapToWikiData(values)
+
+    # create a dictionary at this point with the ids as the keys
+    dictionary = dict(zip(keys, values))
 
     futures = []
-    futures2 = []
-    # making it asynchronous
+    results = []
+
     # https://stackoverflow.com/questions/52082665/store-results-threadpoolexecutor
     with concurrent.futures.ThreadPoolExecutor() as executor:
         for url in keys:
             futures.append(executor.submit(isName, id=url))
 
         for future in concurrent.futures.as_completed(futures):
-            # isName is not identifying the people who are named
-            # apparently not getting any benefit of the parallelism
-            print(future.result())
-            if future.result():
-                futures2.append(url)
+            if future.result()[0]:
+                results.append(future.result()[1])
 
-    print("people who are people:")
-    print(futures2)
-    return keys
+    print("wikidata ids for the items who are people:")
+    print(results)
+
+    res = [dictionary[fut] for fut in results]
+
+    return res
 
 
 if __name__ == "__main__":
     print("Ragoonathachary")
     names = requestPage("https://en.wikipedia.org/wiki/Chinthamani_Ragoonatha_Chary")
     
-    # CANNOT DEAL WITH THE FACT THAT N._R._POGSON AND N.R_.POGSON LINK TO SAME PLACE 
+    # can now deal with unique by the id function but also gives back the wikipedia pages and not the titles
     print(set(names))
-    
-    """
-    f = open("demo2.txt","a")
-    f.truncate(0) 
-    f.write("Source,Target,weight,Type \n")
-    for name in names:
-        f.write("Ragoonathachary, " + name + ", Undirected" + "Ragoonathachary" + "\n")                
-    f.close()  
-    
-    
-    df = pd.read_csv('demo2.txt')
-    G = nx.from_pandas_edgelist(df, 
-                            source='Source',
-                            target='Target',
-                            edge_attr='weight')
+    # would probably be very quick from here to get the titles
 
-    
-    # create vis network
-    from pyvis.network import Network
-
-    net = Network(notebook=True)
-    # load the networkx graph
-    net.from_nx(G)
-    # show
-    net.save_graph("beautiful.html")
-    """
-    
     print("--- %s seconds ---" % (time.time() - start_time))
